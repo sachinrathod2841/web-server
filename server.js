08d175c768d418c63f20d2aff48e179172176a34
Sachin Rathod <sahin.rathod@gmail.com>
2016-02-12 13:10:43 +0530

    update langauge

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(middleware.logger);
 //app.use(middleware.requireAuthentication);
 
 app.get('/about',middleware.requireAuthentication,function (req,res){
-res.send('About Us');
+res.send('About Us!');
 });
 
 
